ec6c4538a64131d6e8894f048a16f860dcb085a9
KreLou <45064938+KreLou@users.noreply.github.com>
2018-12-18 09:02:56 +0100

    Adding Material/Hammer
    Adding HammerJS and Material-Design to app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
