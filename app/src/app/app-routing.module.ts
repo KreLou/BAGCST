4084e26411df1eb6f841a89d0b93538ecd7944f4
KreLou <45064938+KreLou@users.noreply.github.com>
2019-01-07 22:46:28 +0100

    Setup basic
    demo with thomas

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ImprintComponent} from './Global/imprint/imprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'imprint', component: ImprintComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
+import { ImprintComponent } from './Global/imprint/imprint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
