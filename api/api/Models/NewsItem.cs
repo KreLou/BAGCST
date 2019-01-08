8b20a30abaf18e7737d29bfe6ae1947f661393f3
KreLou <45064938+KreLou@users.noreply.github.com>
2019-01-08 20:52:09 +0100

    Created News Interface and class
    Adding INewsDB and NewsItem.cs
    
    Co-Authored-By: Dustin <wized@users.noreply.github.com>

---
+++ api/api/Models/NewsItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class NewsItem
+    {
+        /// <summary>
+        /// unique ID/PK of Post 
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Short header
+        /// max size: 35 char
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Body message of post
+        /// plaintext
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// ID/ForeignKey of posting group
+        /// </summary>
+        public int GroupID { get; set; }
+        /// <summary>
+        /// Timestamp of posting
+        /// </summary>
+        public DateTime Date { get; set; }
+        /*
+         * Possible Extension: automatic posting at specific time
+         */
+    }
+}
