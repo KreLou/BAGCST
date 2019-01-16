b63e5f48c62d95bd847295fc840e39cb6218d861
MariusArlt <46491717+MariusArlt@users.noreply.github.com>
2019-01-16 17:14:49 +0100

    updated ContactsController
    added Get
    added GetAll
    added Edit
    added Delete
    added Create

--- api/api/Controllers/ContactsController.cs
+++ api/api/Controllers/ContactsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using api.Interfaces;
+using api.Models;
 
 namespace api.Controllers
 {
@@ -11,5 +13,102 @@ namespace api.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
+        private IContactsDB database = null;
+
+        /// <summary>
+        /// returns the ContactItem for the given ID. If the ID is not found, it returns NotFound.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ContactItem|NotFound</returns>
+        [HttpGet("{id}")]
+        public ActionResult<ContactItem> getContactItem(int id)
+        {
+            ContactItem item = database.getContactItem(id);
+            if(item==null)
+            {
+                return NotFound($"No ContactItem found for id: {id}");
+            }
+            else
+            {
+                return Ok(item);
+            }
+        }
+
+        /// <summary>
+        /// returns an array of ContactItems
+        /// </summary>
+        /// <returns>ContactItems[]</returns>
+        [HttpGet]
+        public ActionResult<ContactItem[]> getAllContactItems()
+        {
+            ContactItem[] items = database.getAllContactItems();
+            return Ok(items);
+        }
+
+        /// <summary>
+        /// returns the edited ContactItem for the given ID and ContactItem. If ID is not found, it returns NotFound. 
+        /// If the ContactItem is not found, it returns BadRequest.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="item_in"></param>
+        /// <returns>ContactItem|NotFound|BadRequest</returns>
+        [HttpPut("{id}")]
+        public ActionResult<ContactItem> editContactItem(int id, [FromBody]ContactItem item_in)
+        {
+            //TODO check for permission
+            //Check if id is valid
+            if (database.getContactItem(id) == null)
+            {
+                return NotFound(($"No ContactItem found for id: {id}"));
+            }
+
+            //Check if item not null
+            if(item_in == null)
+            {
+                return BadRequest("ContactItem not found");
+            }
+
+            //update existing item
+            ContactItem item_out = database.editContactItem(id, item_in);
+
+            //return new item
+            return Ok(item_out);
+        }
+
+        /// <summary>
+        /// deletes the ContactItem for the given ID. If the ID is not found, it returns NotFound.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public ActionResult deleteContactItem(int id)
+        {
+            //TODO check for permission
+            if (database.getContactItem(id)==null)
+            {
+                return NotFound(($"No ContactItem found for id: {id}"));
+            }
+            database.deleteContactItem(id);
+            return Ok();
+        }
+
+        /// <summary>
+        /// creates a ContactItem based on the given ContactItem. If the given ContactItem is null, it returns BadRequest.
+        /// </summary>
+        /// <param name="item_in"></param>
+        /// <returns>ContactItem|BadRequest</returns>
+        [HttpPost]
+        public ActionResult<ContactItem> createContactItem(ContactItem item_in)
+        {
+            //TODO check for permission
+            if( item_in == null)
+            {
+                return BadRequest("ContactItem not found");
+            }
+            
+            ContactItem item_out = database.createContactItem(item_in);
+            return Created("", item_out);
+        }
+
     }
 }
