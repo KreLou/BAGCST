f8a8215e298ab4107f47cdf0775858eb81e9d204
eliyaalsaad <46280776+eliyaalsaad@users.noreply.github.com>
2019-01-20 10:42:42 +0100

    MenuController
    added MenuController
    Edit IMenuDB

--- api/api/Controllers/MenuController.cs
+++ api/api/Controllers/MenuController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using api.Interfaces;
+
+using api.Models;
 
 namespace api.Controllers
 {
@@ -11,5 +14,100 @@ namespace api.Controllers
     [ApiController]
     public class MenuController : ControllerBase
     {
+        private IMenuDB database = null;
+
+        /// <summary>
+        /// returns the MenuItem for the given ID. If the ID is not found, it returns NotFound.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>MenuItem|NotFound</returns>
+        [HttpGet("{id}")]
+        public ActionResult<MenuItem> GetItem(int id)
+        {
+            MenuItem item = database.GetMenuItem(id);
+
+            if (item == null)
+            {
+                return NotFound($"No MenuItem found for id: {id}");
+            }
+            else
+            {
+                return Ok(item);
+            }
+        }
+        /// <summary>
+        /// returns an array of ContactItems
+        /// </summary>
+        /// <returns>MenuItem[]</returns>
+        [HttpGet]
+        public ActionResult<MenuItem[]> getAllMenuItem(DateTime date)
+        {
+            MenuItem[] items = database.GetMenus(  date);
+            return Ok(items);
+        }
+
+        /// <summary>
+        /// returns the edited MenuItem for the given ID and MenuItem. If ID is not found, it returns NotFound. 
+        /// If the MenuItem is not found, it returns BadRequest.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="menu"></param>
+        /// <returns>MenuItem|NotFound|BadRequest</returns>
+        [HttpPut("{id}")]
+        public ActionResult<MenuItem> editMenuItem(int id, [FromBody]MenuItem menu)
+        {
+            //Check if id is valid
+            if (database.GetMenuItem(id) == null)
+            {
+                return NotFound(($"No MenuItem found for id: {id}"));
+            }
+
+            //Check if item not null
+            if (menu == null)
+            {
+                return BadRequest("MenuItem not found");
+            }
+
+            //update existing item
+            MenuItem menuNew = database.editMenu( menu);
+
+            //return new item
+            return Ok(menuNew);
+        }
+
+        /// <summary>
+        /// deletes the menuNew for the given ID. If the ID is not found, it returns NotFound.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public ActionResult deleteMenuItem(int id)
+        {
+            //TODO check for permission
+            if (database.GetMenuItem(id) == null)
+            {
+                return NotFound(($"No menuNew found for id: {id}"));
+            }
+            database.deleteMenu(id);
+            return Ok();
+        }
+
+        /// <summary>
+        /// creates a MenuItem based on the given MenuItem. If the given MenuItem is null, it returns BadRequest.
+        /// </summary>
+        /// <param name="menu"></param>
+        /// <returns>MenuItem|BadRequest</returns>
+        [HttpPost]
+        public ActionResult<MenuItem> createMenuItem(MenuItem menu)
+        {
+
+            if (menu == null)
+            {
+                return BadRequest("MenuItem not found");
+            }
+
+            MenuItem menuNew = database.saveNewMenu(menu);
+            return Created("", menuNew);
+        }
     }
 }
--- api/api/Interfaces/IMenuDB.cs
+++ api/api/Interfaces/IMenuDB.cs
@@ -8,9 +8,66 @@ namespace api.Interfaces
 {
     interface IMenuDB
     {
+        /// <summary>
+        /// search for MenuItem by ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>MenuItem|null</returns>
+        MenuItem GetMenuItem(int id);
+
+
+        /// <summary>
+        /// creates a MenuItem
+        /// </summary>
+        /// <param name="item">MenuItem</param>
+        /// <returns>full MenuItem</returns>
         MenuItem saveNewMenu(MenuItem item);
+
+        /// <summary>
+        /// edit MenuItem
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>edited MenuItem|null</returns>
         MenuItem editMenu(MenuItem item);
-        MenuItem deleteMenu(MenuItem item);
+
+        /// <summary>
+        /// delete MenuItem
+        /// </summary>
+        /// <param name="id">ID</param>
+        void deleteMenu(int id);
+
+        /// <summary>
+        /// gett all Menu 
+        /// </summary>
+        /// <param name="MenuID">ID</param>
+        /// <param name="Date">Date</param>
+        /// <param name="Meal">Meal</param>
+        /// <param name="Price">Price</param>
+        /// <returns>MenuItem|null</returns>
         MenuItem[] GetMenus(int MenuID, DateTime Date, MealItem Meal, decimal Price);
+
+        /// <summary>
+        /// gett all Menu 
+        /// </summary>
+        /// <param name="MenuID">ID</param>
+        /// <param name="Price">Price</param>
+        /// <returns>MenuItem|null</returns>
+        MenuItem[] GetMenus(int MenuID, DateTime Date);
+
+        /// <summary>
+        /// gett all Menu 
+        /// </summary>
+        /// <param name="Date">Date</param>
+        MenuItem[] GetMenus( DateTime Date);
+
+        /// <summary>
+        /// gett all Menu 
+        /// </summary>
+        /// <param name="Date">Date</param>
+        /// <param name="Meal">Meal</param>
+        /// <param name="Price">Price</param>
+        /// <returns>MenuItem|null</returns>
+        MenuItem[] GetMenus( DateTime Date, MealItem Meal, decimal Price);
+
     }
 }
