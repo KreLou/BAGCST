d320b8856dde29e657f8521aedbdc0b32c1b634a
KreLou <45064938+KreLou@users.noreply.github.com>
2019-02-10 11:19:28 +0100

    Edit Dashboard

--- app/src/app/pages/dashboard/dashboard.page.ts
+++ app/src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuController} from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
+    this.openFirst();
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'dashboard-menu');
+    this.menu.open();
   }
 
 }
