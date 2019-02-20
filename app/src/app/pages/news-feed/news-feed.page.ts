08ce49e1d3ea1b6b736ccf9316f5b3fcfd6af987
KreLou <45064938+KreLou@users.noreply.github.com>
2019-02-20 20:34:05 +0100

    Adding, NewsFeed-, Imprint-, Policy- and About-Pages

---
+++ app/src/app/pages/news-feed/news-feed.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-feed',
+  templateUrl: './news-feed.page.html',
+  styleUrls: ['./news-feed.page.scss'],
+})
+export class NewsFeedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ app/src/app/pages/policy/policy.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-policy',
+  templateUrl: './policy.page.html',
+  styleUrls: ['./policy.page.scss'],
+})
+export class PolicyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
