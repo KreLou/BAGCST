ecee3976ae849be060030ed9de4095c7fb070f85
Louis <45064938+KreLou@users.noreply.github.com>
2019-03-13 13:13:47 +0100

    Create UserDevice with DB

---
+++ api/api/Interfaces/IUserDeviceDB.cs
@@ -0,0 +1,16 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    interface IUserDeviceDB
+    {
+        UserDeviceItem createNewUserDevice(UserDeviceItem item);
+        UserDeviceItem getDeviceByNameAndUser(long userID, string deviceName);
+
+        UserDeviceItem[] getAllUserDeviceItems();
+    }
+}
