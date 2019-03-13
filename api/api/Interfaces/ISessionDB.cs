e535d56490161d6e8b2beb95b5179bf41e9ae81f
Louis <45064938+KreLou@users.noreply.github.com>
2019-03-13 13:14:01 +0100

    Create Session with DB

---
+++ api/api/Interfaces/ISessionDB.cs
@@ -0,0 +1,19 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    interface ISessionDB
+    {
+        SessionItem findExistingSession(long userID, long deviceID);
+
+        SessionItem createNewSession(SessionItem item);
+
+        SessionItem[] getAllSessions();
+
+        SessionItem[] getAllActiveSessions();
+    }
+}
---
+++ api/api/Models/SessionItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class SessionItem
+    {
+        public string Token { get; set; }
+        public long InternalID { get; set;}
+        public long DeviceID { get; set; }
+        public long UserID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        
+
+    }
+}
