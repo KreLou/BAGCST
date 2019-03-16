aa4ef6389acb666f0df1986644bd8dc24f0bc068
KreLou <45064938+KreLou@users.noreply.github.com>
2019-03-16 14:13:45 +0100

    Add GlobalHTTPOptions

---
+++ app/src/app/services/global-http.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {TokenService} from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalHTTPService {
+
+  constructor(private tokenService: TokenService) { }
+
+  public AnonymousHTTPOptions = {
+    'Content-Type': 'application/json'
+  };
+
+  public AuthorizedHTTPOptions = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + this.tokenService.getToken()
+  };
+}
