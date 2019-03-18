76dc6a2dcf1d9d221e2c774c88facc48502e8de7
Louis <45064938+KreLou@users.noreply.github.com>
2019-03-18 09:55:23 +0100

    Add Lecture (Model, DB, Interface)

---
+++ api/api/Interfaces/ITimetableDB.cs
@@ -0,0 +1,28 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    interface ITimetableDB
+    {
+        /// <summary>
+        /// Returns the Lecture from specific semester
+        /// Starttime should be the beginning of the current semester
+        /// </summary>
+        /// <param name="studyGroup"></param>
+        /// <param name="semester"></param>
+        /// <returns></returns>
+        LectureItem[] getSemesterLectures(string studyGroup, DateTime startTime);
+
+        /// <summary>
+        /// Get the Lectures for the Lecturer
+        /// </summary>
+        /// <param name="lecturer"></param>
+        /// <param name="startTime"></param>
+        /// <returns></returns>
+        LectureItem[] getLecturesByLecturer(string lecturer, DateTime startTime);
+    }
+}
---
+++ api/api/Models/LectureItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class LectureItem
+    {
+        public string StudyGroup { get; set; }
+        public int Semester { get; set; }
+        public string Place { get; set; }
+        public string Title { get; set; }
+        public string Lecturer { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
