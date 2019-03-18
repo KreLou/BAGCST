53801e10a55841cf1bb26f970f3c2e7611173ecc
Louis <45064938+KreLou@users.noreply.github.com>
2019-03-18 09:55:37 +0100

    Add Semester (Model, DB, Interface)

---
+++ api/api/Interfaces/ISemesterDB.cs
@@ -0,0 +1,14 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    interface ISemesterDB
+    {
+        SemesterItem[] getSemesterItem(string studyGroup);
+        SemesterItem getCurrentSemesterByStudyGroup(string studyGroup);
+    }
+}
