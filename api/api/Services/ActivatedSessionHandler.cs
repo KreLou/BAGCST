609b84fdfbaf6605adc4206b43484b19e5d971d7
Dustin <45274946+Wized@users.noreply.github.com>
2019-03-27 21:59:50 +0100

    check session
    checks session is actived

--- api/api/Services/ActivatedSessionHandler.cs
+++ api/api/Services/ActivatedSessionHandler.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Selectors;
+using api.Models;
+using System.Linq;
 
 namespace api.Services
 {
@@ -17,8 +20,34 @@ namespace api.Services
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ActivatedSessionRequirement requirement)
         {
-            //TODO Check if Session is active
-            context.Succeed(requirement);
+            long _sessionID = long.MinValue;
+            SessionItem _sessionItem = null;
+            bool _success = true;
+
+            foreach (var claim in context.User.Claims.ToList())
+            {
+                if (claim.Type == TokenFields.SessionID)
+                {
+                   _sessionID = Convert.ToInt64(claim.Value); 
+                }
+            }
+            _sessionItem = _sessionDB.getSessionByInternalID(_sessionID);
+            
+            _success = _sessionItem != null;
+            _success = (_sessionItem.ExpirationTime >= DateTime.Now && _sessionItem.StartTime <= DateTime.Now);
+            _success = _sessionItem.isActivied;
+            // TODO krelou should we compare userIDs...?
+            // _success = _sessionItem.UserID == context.User.Claims.
+
+            if (_success)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+            
             return Task.CompletedTask;
         }
     }
