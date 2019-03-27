da6b3d7e13e9d9d5804276aa13473aab8d9321a6
Dustin <45274946+Wized@users.noreply.github.com>
2019-03-28 00:23:53 +0100

    Created TokenInformation
    Init object TokenInformation as persisten runtime storage

---
+++ api/api/Selectors/TokenInformation.cs
@@ -0,0 +1,11 @@
+namespace api.Selectors
+{
+    public class TokenInformation
+    {
+        public static string Username { get; set;}
+        public static string Firstname {get; set;}
+        public static string Lastname { get; set;}
+        public static string DeviceID { get; set;}
+        public static string SessionID { get; set;}
+    }
+}
