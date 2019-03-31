b92b5bbe00ab68aec795fc8ec2cd669c8cb1b669
Louis <45064938+KreLou@users.noreply.github.com>
2019-03-31 12:01:28 +0200

    Create MEController.cs

---
+++ api/api/Controllers/MEController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.offlineDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MEController : ControllerBase
+    {
+        private IUserDB userDB;
+        private IUserSettingsDB userSettingsDB;
+        
+        public MEController()
+        {
+            this.userDB = new offlineUserDB();
+            this.userSettingsDB = new offlineUserSettings();
+        }
+
+        [HttpGet("settings")] 
+        public IActionResult getSettings()
+        {
+            long userID = 1; //TODO Get From Token
+
+            return Ok();
+        }
+    }
+}
