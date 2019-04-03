f45df47827952663b203a40356391254cba581f4
Louis <45064938+KreLou@users.noreply.github.com>
2019-04-03 18:50:48 +0200

    Create DuplicateRightsItemFoundException.cs

---
+++ api/api/Exception/DuplicateRightsItemFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Exception
+{
+    public class DuplicateRightsItemFoundException: System.Exception
+    {
+        public int RightID { get; private set; }
+        public DuplicateRightsItemFoundException(int rightID) : base()
+        {
+            this.RightID = rightID;
+        }
+    }
+}
