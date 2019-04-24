0cfc5156b54a5b6e9044d073c5398b202b1d52a6
StevO <little_white_wulf@web.de>
2019-04-24 19:19:30 +0200

    Click Dummy 2
    Fortsetzung

--- app/src/app/pages/food-menu/food-menu.page.ts
+++ app/src/app/pages/food-menu/food-menu.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-menu.page.css'],
 })
 export class FoodMenuPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  currentDate;
+  formattedDate
+  constructor() {
+    this.currentDate = new Date()
+    this.getFormattedDate()
   }
+  getFormattedDate(){
+    var dateObj = new Date()
 
+    var year = dateObj.getFullYear().toString()
+    var month = dateObj.getMonth().toString()
+    var date = dateObj.getDate().toString()
+
+    var monthArray = ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember']
+
+    this.formattedDate = date + '. ' + monthArray[month] + ' ' + year
+  }
+  ngOnInit() {}
 }
