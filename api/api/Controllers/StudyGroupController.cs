245a42b1769c9e028fad6d97b497e8faa1a1efa6
KreLou <45064938+KreLou@users.noreply.github.com>
2019-05-01 13:01:28 +0200

    Create Controller

---
+++ api/api/Controllers/StudyGroupController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+using api.offlineDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudyGroupController : ControllerBase
+    {
+        private readonly IStudyGroupDB studyGroupDB = new offlineStudyGroupDB();
+
+        [HttpGet]
+        public StudyGroup[] getAll()
+        {
+            return studyGroupDB.getAll();
+        }
+    }
+}
