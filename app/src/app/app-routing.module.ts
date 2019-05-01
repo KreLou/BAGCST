8dee2cc08ec0d7206c81393456ab53c452349fbd
Louis <louis.kretzschmann@gmail.com>
2019-05-02 01:04:09 +0200

    Generate AdminFoodPlaner.Page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -13,8 +13,10 @@ const routes: Routes = [
 	{ path: 'imprint', loadChildren: './pages/imprint/imprint.module#ImprintPageModule' },
 	{ path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
 	{ path: 'privacy', loadChildren: './pages/privacy/privacy.module#PrivacyPageModule' },
+  { path: 'admin-food-planer', loadChildren: './pages/admin-food-planer/admin-food-planer.module#AdminFoodPlanerPageModule' },
 
-	{ path: '**', redirectTo: 'dashboard'}
+	{ path: '**', redirectTo: 'dashboard'},
+
 
 ];
 @NgModule({
---
+++ app/src/app/pages/admin-food-planer/admin-food-planer.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-food-planer',
+  templateUrl: './admin-food-planer.page.html',
+  styleUrls: ['./admin-food-planer.page.css'],
+})
+export class AdminFoodPlanerPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
