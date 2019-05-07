bc9952eb5f092be25409a42a1116441b5c74e18f
MariusArlt <46491717+MariusArlt@users.noreply.github.com>
2019-05-07 22:27:44 +0200

    INIT ContactLoader
    ADD ContactLoader
    ADD ContactModel
    ADD Environment

--- app/src/app/pages/contacts/contacts.page.ts
+++ app/src/app/pages/contacts/contacts.page.ts
@@ -3,39 +3,44 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import {Injectable} from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-@Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.page.html',
-  styleUrls: ['./contacts.page.css'],
-})
 
-@NgModule({
-  declarations:[],
-  imports: [BrowserModule, HttpClientModule]
+@Injectable({
+  providedIn: 'root'
 })
 
 export class ContactsPage implements OnInit {
 
-  constructor() {
-    // let Testitem:IonItem = document.element getElementById('testID')
-    // var TestVariable:string = 'TestText';
-    // Testitem = TestVariable;
-    
-  }
-  
-
+  constructor(
+    private contactLoader: ContactLoaderService
+  ) {}
   ngOnInit() {
+    this.contactLoader.getAllContacts().subscribe(data => console.log(data));
   }
 
-
-
-}
-@Injectable()
-export class Configuration{
-  public server = 'http://localhost:50179/';
-  public apiUrl = 'api/';
-  public serverWithApiUrl = this.server + this.apiUrl;
 }
 
-export var contactList:string[] = ["TestItem1", "TestItem2", "TestItem3"];
+// export let contactList: string[] = ['TestItem1', 'TestItem2', 'TestItem3'];
+
+export interface Contact {
+  contactID: number;
+  firstName: string;
+  lastName: string;
+  telNumber: string;
+  email: string;
+  room: string;
+  responsibility: string;
+  course: string;
+  type: string;
+}
+
+export class ContactLoaderService {
+  constructor(private http: HttpClient) {}
+
+  getAllContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(environment.apiURL + `/api/contacts`);
+  }
+}
