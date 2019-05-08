c116c968cbe5ce18acf685443e87ad077b565495
Louis <45064938+KreLou@users.noreply.github.com>
2019-05-08 11:24:11 +0200

    Update and rename ContactModel.ts to ContactItem.ts

---
+++ app/src/app/models/ContactItem.ts
@@ -0,0 +1,13 @@
+
+export interface ContactItem {
+    contactID: number;
+    firstName: string;
+    lastName: string;
+    telNumber: string;
+    email: string;
+    room: string;
+    responsibility: string;
+    course: string;
+    type: string;
+
+}
