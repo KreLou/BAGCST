61cefd6dc5f566394592a7f5c78b1ed98407005a
MariusArlt <46491717+MariusArlt@users.noreply.github.com>
2019-05-08 18:43:28 +0200

    Update ContactList
    ADD contactList and used it

--- app/src/app/pages/contacts/contacts.page.ts
+++ app/src/app/pages/contacts/contacts.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Injectable} from '@angular/core';
 import { ContactsLoaderService } from 'src/app/services/httpServices/contacts-loader.service';
+import { ContactItem } from 'src/app/models/ContactItem';
 
 
 @Component({
@@ -8,14 +9,18 @@ import { ContactsLoaderService } from 'src/app/services/httpServices/contacts-lo
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.css'],
 })
+
 export class ContactsPage implements OnInit {
+
+  private contactList: ContactItem[];
 
   constructor(
     private contactLoader: ContactsLoaderService
   ) {}
 
   ngOnInit() {
-    this.contactLoader.getAllContacts().subscribe(data => {console.log(data)}, error => {console.error(error);});
+    this.contactLoader.getAllContacts().subscribe(data => {console.log(data);}, error => {console.error(error); });
+    this.contactLoader.getAllContacts().subscribe(data => {this.contactList = data;});    
   }
 
 }
