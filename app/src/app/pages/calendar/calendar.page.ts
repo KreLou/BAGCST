57c844fd3003002ce62f10c73ec12b90c242a0ab
Louis <louis.kretzschmann@gmail.com>
2019-05-11 23:52:02 +0200

    Small Fixes

--- app/src/app/pages/calendar/calendar.page.ts
+++ app/src/app/pages/calendar/calendar.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TimetableItem } from "../../models/TimetableItem";
 import {TimetableLoaderService} from 'src/app/services/httpServices/timetable-loader.service';
 import {expand} from 'rxjs/operators';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-calendar',
@@ -9,22 +10,32 @@ import {expand} from 'rxjs/operators';
   styleUrls: ['./calendar.page.css'],
 })
 export class CalendarPage implements OnInit {
+
+    noMoreLectureItemsToShow = false;
+
     today: Date;
     enddate: Date;
     listUpcomingLectures: any;
     listTimetable: TimetableItem[];
     displayTimetable: any;
-    constructor(private timetableloader: TimetableLoaderService){}
-  ngOnInit(): void {
+
+
+    CONST_HowManyItemsAddedAtEndOfView = 3;
+    CONST_HowManyMilliSecondsPerDay = 1000 * 60 * 60 * 24;
+
+    constructor(private timetableloader: TimetableLoaderService){
         this.today = new Date();
         this.today.setHours(0,0,0,0);
-        this.enddate = new Date(this.today.getTime() + (1000 * 60 * 60 * 24 * 8));
+        this.enddate = new Date(this.today.getTime() + (this.CONST_HowManyMilliSecondsPerDay * 8));
+    }
+
+  ngOnInit(): void {
         this.timetableloader.getTimetable().subscribe(data => {
-            console.table(data);
+
             this.listTimetable = data;
             this.displayTimetable = new Array();
             var days = Array.from(new Set(this.listTimetable.map(x => x.start.toString().split('T')[0])));
-            console.log('days', days);
+
             days.forEach(date => {
                 this.displayTimetable.push({
                     date: new Date(date),
@@ -35,23 +46,33 @@ export class CalendarPage implements OnInit {
             this.listUpcomingLectures = this.displayTimetable.filter(x => x.date >= this.today);
             this.displayTimetable = this.listUpcomingLectures.filter(x => x.date <= this.enddate);
             this.displayTimetable[0].expand = true;
-            console.log('this.displayTimeTable', this.displayTimetable);
         });
   }
   changeExpandable(day: any) {
         console.log(day);
         day.expand = !day.expand;
   }
+
+  /**
+   * Loads more data into displayTimeTable
+   * @author KreLou
+   * @param event 
+   */
   loadData(event: any) {
+    /**use timeout, to display the laoding spinner for show time */  
+    setTimeout(() => {
         const dates = this.displayTimetable.map(x => x.date);
         const lastday = dates[dates.length - 1];
         const nextElements = this.listUpcomingLectures.filter(x => x.date > lastday);
-        for (var i = 0; i < 3; i++){
+        for (var i = 0; i < this.CONST_HowManyItemsAddedAtEndOfView; i++){
             if (nextElements[i]){
                 this.displayTimetable.push(nextElements[i]);
+            } else {
+                this.noMoreLectureItemsToShow = true;
             }
         }
-        console.log(lastday);
+        event.target.complete();
+    }, 500);
   }
 }
 
--- app/src/app/services/httpServices/timetable-loader.service.ts
+++ app/src/app/services/httpServices/timetable-loader.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {TimetableItem} from "../../models/TimetableItem";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class TimetableLoaderService {
   constructor(private http: HttpClient) { }
 
   public getTimetable(): Observable<TimetableItem[]> {
-    return this.http.get<TimetableItem[]>('http://172.17.17.141:5000/api/Timetable');
+    return this.http.get<TimetableItem[]>(environment.apiURL + '/api/timetable');
   }
 }
