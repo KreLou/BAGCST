974449bf4a263b457c3b54cb8b52cb03959e8813
Louis <louis.kretzschmann@gmail.com>
2019-05-13 20:40:46 +0200

    Rename

--- api/api/database/Database.cs
+++ api/api/database/Database.cs
@@ -9,8 +9,13 @@ namespace api.database
     public static class TimeTableDatabase
     {
         //private static string connectionString = "Data Source =192.168.99.123; Initial Catalog = schulessen; User ID = sa; Password=Viper001!";
+<<<<<<< HEAD:api/api/database/TimeTableDatabase.cs
         private static string connectionString = "Data Source =192.168.123.112; Initial Catalog = Stundenplan; User ID = TestLouis; Password=Louis";
         private static string connectionString_timetable = "Data Source =192.168.123.112; Initial Catalog = Stundenplan-Original; User ID = TestLouis; Password=Louis";
+=======
+        private static string connectionString = "Data Source =192.168.99.129; Initial Catalog = Stundenplan; User ID = sa; Password=Viper001!";
+        private static string connectionString_timetable = "Data Source =192.168.99.129; Initial Catalog = Stundenplan-Original; User ID = sa; Password=Viper001!";
+>>>>>>> parent of 1424380... Rename Database > TimeTableDatabase:api/api/database/Database.cs
         private static List<SqlConnection> connections = new List<SqlConnection>();
         public static void setConnectionString(string str)
         {
