757cea713c734e55417685a3018024092fd3cb68
ThShepard <46451794+ThShepard@users.noreply.github.com>
2019-05-16 19:39:26 +0200

    Little Design Tweaks

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
-  {path: 'administrator', loadChildren: './pages/administrator/administrator.module#AdministratorPageModule'},
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
+  { path: 'administrator', loadChildren: './pages/administrator/administrator.module#AdministratorPageModule'},
   { path: 'settings', loadChildren: './../pages/settings/settings.module#SettingsPageModule' },
   { path: 'login', loadChildren: './../pages/login/login.module#LoginPageModule' },   
   { path: 'imprint', loadChildren: './../pages/imprint/imprint.module#ImprintPageModule' },
