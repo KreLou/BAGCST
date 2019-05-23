732bbf10699a3be8dca181f0295586b226fb54e1
ThShepard <46451794+ThShepard@users.noreply.github.com>
2019-05-23 11:48:02 +0200

    routing bugfix

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
   {path: 'administrator', loadChildren: './pages/administrator/administrator.module#AdministratorPageModule'},
-  { path: 'settings', loadChildren: './../pages/settings/settings.module#SettingsPageModule' },
-  { path: 'login', loadChildren: './../pages/login/login.module#LoginPageModule' },   
-  { path: 'imprint', loadChildren: './../pages/imprint/imprint.module#ImprintPageModule' },
-  { path: 'about', loadChildren: './../pages/about/about.module#AboutPageModule' },
-  { path: 'privacy', loadChildren: './../pages/privacy/privacy.module#PrivacyPageModule' },
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },   
+  { path: 'imprint', loadChildren: './pages/imprint/imprint.module#ImprintPageModule' },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'privacy', loadChildren: './pages/privacy/privacy.module#PrivacyPageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
