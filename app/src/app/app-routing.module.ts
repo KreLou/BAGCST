09f6ef6ca84a82953971c8ecdf9a215147758ae5
ThShepard <46451794+ThShepard@users.noreply.github.com>
2019-06-06 10:25:16 +0200

    undo routing and API host

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { ActivatedRouteGuard } from './guards/activated-route.guard';
 
 const routes: Routes = [
 
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
-  { path: 'administrator', loadChildren: './pages/administrator/administrator.module#AdministratorPageModule'},
-  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard, ActivatedRouteGuard]},
+  { path: 'administrator', loadChildren: './pages/administrator/administrator.module#AdministratorPageModule', canActivate: [AuthGuard, ActivatedRouteGuard]},
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule', canActivate: [AuthGuard, ActivatedRouteGuard] },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },   
   { path: 'activate/:code', loadChildren: './pages/activate-code/activate-code.module#ActivateCodePageModule' },
   { path: 'imprint', loadChildren: './pages/imprint/imprint.module#ImprintPageModule' },
